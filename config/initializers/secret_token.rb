924e59937c1ecd8438aec7fe2b4a259601a2f77a
sora sakaki <sora.sakaki@gmail.com>
2013-04-19 21:34:41 +0900

    Railsアプリケーションを作成した

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ariake::Application.config.secret_token = '6e42bdd0e0326cea42f613c2798b3169f7829d1dbd7a0e18e90cbeaa2cd2f7f2420193ac2ee0179d7131d2c1f3f2c01545ba3d62faa8b9e48b5772aec80190fe'
