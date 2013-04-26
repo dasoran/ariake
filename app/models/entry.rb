ae26441d134dad362dbb6296c399db6d8a1e6942
sora sakaki <sora.sakaki@gmail.com>
2013-04-27 00:19:01 +0900

    Modelのバグを修正

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ActiveRecord::Base
   attr_accessible :event_id, :circle_id, :attend_at, :place, :goods_updated_at
   belongs_to :event
   belongs_to :circle
-  has_many :circle_goods_lists
+  has_many :handouts
   before_create :set_goods_updated_at_to_now
   def set_goods_updated_at_to_now
     self.goods_updated_at = Time.now
