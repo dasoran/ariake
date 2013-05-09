f351ac99934c8149753426a01aef4c7b64d5d606
xxYuiyaxx <xxyuiyaxx.rain.8492@gmail.com>
2013-05-09 23:30:20 +0900

    entry画面に更新日時を表示するようにした

---
+++ app/controllers/checklists_controller.rb
@@ -0,0 +1,6 @@
+class ChecklistsController < ApplicationController
+  def index
+  end
+  def show
+  end
+end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,5 +3,26 @@ class EntriesController < ApplicationController
   def index
     event = Event.find_by_name("C84") 
     @entries = Entry.find_all_by_event_id(event.id)
-   end 
+    def search
+    end
+  end 
+  
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
