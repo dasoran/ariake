0d3fc1f537e7a03b184f90546124b4a35a1e7338
sora sakaki <sora.sakaki@gmail.com>
2013-07-11 23:34:13 +0900

    map に日付変更ボタン、会場変更ボタンをつけた。

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -11,10 +11,10 @@ class MapsController < ApplicationController
   def index
     area_to_str = { "e" => "東", "w" => "西" }
 
-    day = params[:day] || "1"
-    area = params[:area] || "e"
-    subarea = params[:subarea] || "123"
-    map_id = day + area + subarea
+    @day = params[:day] || "1"
+    @area = params[:area] || "e"
+    @subarea = params[:subarea] || "123"
+    map_id = @day + @area + @subarea
     @map_id = map_id.upcase
 
     event_name = params[:event] || "c84"
@@ -27,9 +27,9 @@ class MapsController < ApplicationController
         if handout.orders.count != 0 then
           block = entry.map_layout.comiket_block
           carea = block.comiket_area
-          next if entry.attend_at != day.to_i # 該当日に参加しているか
-          next if area_to_str[area] != carea.name[0] 
-          next if subarea.to_i != carea.map_id
+          next if entry.attend_at != @day.to_i # 該当日に参加しているか
+          next if area_to_str[@area] != carea.name[0] 
+          next if @subarea.to_i != carea.map_id
 
           exe_users = []
           handout.orders.each do |order|
@@ -52,10 +52,10 @@ class MapsController < ApplicationController
 
     executor = User.find_by_login_id params[:id]
 
-    day = params[:day] || "1"
-    area = params[:area] || "e"
-    subarea = params[:subarea] || "123"
-    map_id = day + area + subarea
+    @day = params[:day] || "1"
+    @area = params[:area] || "e"
+    @subarea = params[:subarea] || "123"
+    map_id = @day + @area + @subarea
     @map_id = map_id.upcase
 
     event_name = params[:event] || "c84"
@@ -67,10 +67,10 @@ class MapsController < ApplicationController
       catch(:exit) {
         entry.handouts.each do |handout|
           if handout.orders.count != 0 then
-            next if entry.attend_at != day.to_i # 該当日に参加しているか
-            next if area_to_str[area] != entry.place[0]
+            next if entry.attend_at != @day.to_i # 該当日に参加しているか
+            next if area_to_str[@area] != entry.place[0]
             area_id = @@comiket_block[entry.place[1]]
-            next if subarea != @@comiket_area[area_id]["mapId"]
+            next if @subarea != @@comiket_area[area_id]["mapId"]
             
             exe_users = []
             # 購入してくる人で絞り込み
